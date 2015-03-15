711e6c7ee5d5e509d5515793e40288872a68a440
Andrei <aformusatii@gmail.com>
2015-03-15 22:53:27 +0200

    Deleted old files.

--- proc_stat/build.sh
+++
@@ -1,2 +0,0 @@
-gcc proc_stat.c -o proc_stat
-cp proc_stat /usr/lib/cgi-bin/
--- proc_stat/cd_proc_stat.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$output = shell_exec('cat /proc/stat');
-?>
-
-<?php echo $_GET['callback']; ?>('<?php echo str_replace("\n", "|", "$output"); ?>');
